e179cf9b7dce5e4cf39e95db537184cba9449f5d
Ravneet Punia <ravneet.dtu@Gmail.com>
2017-06-30 22:55:21 +0530

    2nd App using multiple Intents and Fetching Data And Images, showing them in Various Activity

---
+++ Fetching_JSON_Data/app/src/main/java/com/example/ravneet/fetching_json_data/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.ravneet.fetching_json_data;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageView user,post,album,todo;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ((ImageView)findViewById(R.id.img_user)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this,UserActivity.class));
+            }
+        });
+
+        ((ImageView)findViewById(R.id.img_album)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this,AlbumActivity.class));
+            }
+        });
+
+        ((ImageView)findViewById(R.id.img_post)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this,PostActivity.class));
+            }
+        });
+
+        ((ImageView)findViewById(R.id.img_todo)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this,ToDoActivity.class));
+            }
+        });
+    }
+}
