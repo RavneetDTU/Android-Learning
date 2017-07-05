ca527d93c3d09b7e3b20bcdcab9827d81b9673d7
Ravneet Punia <ravneet.dtu@Gmail.com>
2017-07-05 23:42:25 +0530

    Message Sending and Showing on MainScreen

--- FirebaseApp/app/src/main/java/com/example/ravneet/firebaseapp/MainActivity.java
+++ FirebaseApp/app/src/main/java/com/example/ravneet/firebaseapp/MainActivity.java
@@ -18,6 +18,12 @@ import android.widget.ProgressBar;
 import com.example.ravneet.firebaseapp.FriendlyMessage;
 import com.example.ravneet.firebaseapp.MessageAdapter;
 import com.example.ravneet.firebaseapp.R;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -38,12 +44,21 @@ public class MainActivity extends AppCompatActivity {
 
     private String mUsername;
 
+    private FirebaseDatabase mFirebaseDatabase;
+    private DatabaseReference mDatabaseReference;
+    private ChildEventListener mChildEventListner;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         mUsername = ANONYMOUS;
+
+        mFirebaseDatabase = FirebaseDatabase.getInstance();
+        mDatabaseReference = mFirebaseDatabase.getReference().child("message");
+
+
 
         // Initialize references to views
         mProgressBar = (ProgressBar) findViewById(R.id.progressBar);
@@ -93,12 +108,43 @@ public class MainActivity extends AppCompatActivity {
         mSendButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                // TODO: Send messages on click
-
-                // Clear input box
+                FriendlyMessage friendlyMessage = new FriendlyMessage(mMessageEditText.getText().toString(),mUsername,null);
+                mDatabaseReference.push().setValue(friendlyMessage);
                 mMessageEditText.setText("");
             }
         });
+
+        mChildEventListner = new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+
+                FriendlyMessage friendlyMessage = dataSnapshot.getValue(FriendlyMessage.class);
+                mMessageAdapter.add(friendlyMessage);
+
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        };
+
+        mDatabaseReference.addChildEventListener(mChildEventListner);
     }
 
     @Override
