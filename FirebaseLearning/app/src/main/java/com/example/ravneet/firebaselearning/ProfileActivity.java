01c364ec74aba219ffa9c4ca0e2344654ee59b02
Ravneet Punia <ravneet.dtu@Gmail.com>
2017-07-10 15:28:50 +0530

    Uploading And Editing User Information on Firebase

--- FirebaseLearning/app/src/main/java/com/example/ravneet/firebaselearning/ProfileActivity.java
+++ FirebaseLearning/app/src/main/java/com/example/ravneet/firebaselearning/ProfileActivity.java
@@ -5,17 +5,23 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class ProfileActivity extends AppCompatActivity {
 
     TextView tv_username;
-    Button btn_logout;
+    Button btn_logout,btn_adddata;
+    EditText et_name,et_adderss;
 
     private FirebaseAuth firebaseAuth;
+    private DatabaseReference databaseReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +30,9 @@ public class ProfileActivity extends AppCompatActivity {
 
         tv_username = (TextView) findViewById(R.id.tv_useremail);
         btn_logout = (Button) findViewById(R.id.btn_logout);
+        btn_adddata = (Button) findViewById(R.id.btn_addData);
+        et_name = (EditText) findViewById(R.id.et_name);
+        et_adderss = (EditText) findViewById(R.id.et_address);
 
         firebaseAuth = FirebaseAuth.getInstance();
 
@@ -32,8 +41,10 @@ public class ProfileActivity extends AppCompatActivity {
             finish();
         }
 
+        databaseReference = FirebaseDatabase.getInstance().getReference();
+
         FirebaseUser user = firebaseAuth.getCurrentUser();
-        tv_username.setText("Welcome...... "+user.getEmail());
+        tv_username.setText("Welcome..... "+user.getEmail());
 
         btn_logout.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -44,6 +55,28 @@ public class ProfileActivity extends AppCompatActivity {
             }
         });
 
+        btn_adddata.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                saveuserInformation();
+            }
+        });
+
+
+    }
+
+    private void saveuserInformation(){
+        String name = et_name.getText().toString().trim();
+        String address = et_adderss.getText().toString().trim();
+
+        UserInformation userInformation = new UserInformation(name,address);
+
+        // Now we need Unique ID of Loggged In User
+        FirebaseUser user = firebaseAuth.getCurrentUser();
+
+        databaseReference.child(user.getUid()).setValue(userInformation);
+
+        Toast.makeText(this,"Information Saved....",Toast.LENGTH_SHORT).show();
 
     }
 }
---
+++ FirebaseLearning/app/src/main/java/com/example/ravneet/firebaselearning/UserInformation.kt
@@ -0,0 +1,9 @@
+package com.example.ravneet.firebaselearning
+
+/**
+ * Created by ravneet on 10/7/17.
+ */
+data class UserInformation(
+        val name: String,
+        val address: String
+)
