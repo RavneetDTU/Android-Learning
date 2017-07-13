84bb712530ca5bce68eb32fd974c9d7cb8d01e49
Ravneet Punia <ravneet.dtu@Gmail.com>
2017-07-13 12:00:30 +0530

    Simple Project on Location Access

---
+++ Location/app/src/main/java/com/example/ravneet/location/MainActivity.java
@@ -0,0 +1,86 @@
+package com.example.ravneet.location;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    LocationManager locMan;
+    LocationListener locLis;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        locMan = (LocationManager) getSystemService(LOCATION_SERVICE);
+        locLis = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+
+            }
+
+            @Override
+            public void onStatusChanged(String s, int i, Bundle bundle) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String s) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String s) {
+
+            }
+        };
+
+        if ((ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) &&
+
+                (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
+                        != PackageManager.PERMISSION_GRANTED)) {
+
+            ActivityCompat.requestPermissions(this,
+                    new String[] {
+                            Manifest.permission.ACCESS_COARSE_LOCATION,
+                            Manifest.permission.ACCESS_FINE_LOCATION
+                    }, 234);
+        } else {
+            startLocationTracking();
+        }
+
+    }
+
+    @SuppressWarnings("MissingPermission")
+    void startLocationTracking () {
+
+        locMan.requestLocationUpdates(
+                LocationManager.NETWORK_PROVIDER,
+                10000,
+                10,
+                locLis
+        );
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (permissions[0].equals(Manifest.permission.ACCESS_COARSE_LOCATION)) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                startLocationTracking();
+            }
+        }
+    }
+
+}
+
