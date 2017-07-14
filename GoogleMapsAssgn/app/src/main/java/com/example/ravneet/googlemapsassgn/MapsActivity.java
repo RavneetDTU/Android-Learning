58c1252b2867d56091623f5c3ebe5f23f84a8704
Ravneet Punia <ravneet.dtu@Gmail.com>
2017-07-15 01:59:11 +0530

    Marker For perticular postion added

--- GoogleMapsAssgn/app/src/main/java/com/example/ravneet/googlemapsassgn/MapsActivity.java
+++ GoogleMapsAssgn/app/src/main/java/com/example/ravneet/googlemapsassgn/MapsActivity.java
@@ -1,7 +1,15 @@
 package com.example.ravneet.googlemapsassgn;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -10,9 +18,14 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.io.IOException;
+import java.util.List;
+
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    EditText et_location;
+    Button btn_search;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,8 +35,44 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+        et_location = (EditText) findViewById(R.id.et_location);
+        btn_search = (Button) findViewById(R.id.btn_search);
+
+
+        btn_search.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                List<Address> addressList = null;
+
+                String location = et_location.getText().toString().trim();
+                if(location != null || !location.equals("")){
+
+                    Geocoder geocoder = new Geocoder(getApplicationContext());
+                    try {
+                        addressList = geocoder.getFromLocationName(location,1);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+
+                    Address address = addressList.get(0);
+
+                    LatLng latLng = new LatLng(address.getLatitude(),address.getLongitude());
+
+                    mMap.addMarker(new MarkerOptions().position(latLng).title("Marker On Your Location"));
+                    mMap.animateCamera(CameraUpdateFactory.newLatLng(latLng));
+                }
+            }
+        });
+
+
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        onMapReady(mMap);
+    }
 
     /**
      * Manipulates the map once available.
@@ -42,6 +91,16 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         LatLng sydney = new LatLng(-34, 151);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
         mMap.setMyLocationEnabled(true);
     }
 }
