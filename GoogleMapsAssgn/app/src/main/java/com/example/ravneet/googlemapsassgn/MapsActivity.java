196a0cdbf6aa36b131466c97fd2d1bed0565132a
Ravneet Punia <ravneet.dtu@Gmail.com>
2017-07-15 23:09:20 +0530

    Layout set with ZoomIn/ZoomOut Buttons

--- GoogleMapsAssgn/app/src/main/java/com/example/ravneet/googlemapsassgn/MapsActivity.java
+++ GoogleMapsAssgn/app/src/main/java/com/example/ravneet/googlemapsassgn/MapsActivity.java
@@ -4,28 +4,49 @@ import android.Manifest;
 import android.content.pm.PackageManager;
 import android.location.Address;
 import android.location.Geocoder;
+import android.location.LocationListener;
+import android.os.Build;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+import android.widget.ZoomControls;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PolylineOptions;
 
 import java.io.IOException;
 import java.util.List;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
 
     private GoogleMap mMap;
-    EditText et_location;
-    Button btn_search;
+    EditText et_location, et_destination;
+    Button btn_search,btn_mark;
+    ZoomControls zoom;
+    Double myLatitude = null;
+    Double myLongitude = null;
+
+    private GoogleApiClient googleApiClient;
+    // For LocationRequest check depandency of location
+    private LocationRequest locationRequest;
+
+    public static final int MY_PERMISSION_FINE_LOCATION = 101;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,38 +56,76 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        googleApiClient = new GoogleApiClient
+                .Builder(this)
+                .addApi(LocationServices.API)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .build();
+
+        locationRequest = new LocationRequest(this);
+        locationRequest.setInterval(15*1000);
+        locationRequest.setFastestInterval(5*1000);
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+
         et_location = (EditText) findViewById(R.id.et_location);
         btn_search = (Button) findViewById(R.id.btn_search);
+        btn_mark = (Button) findViewById(R.id.btn_mark);
+        zoom = (ZoomControls) findViewById(R.id.zoom);
 
+        zoom.setOnZoomInClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mMap.animateCamera(CameraUpdateFactory.zoomIn());
+            }
+        });
+
+        zoom.setOnZoomOutClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mMap.animateCamera(CameraUpdateFactory.zoomOut());
+            }
+        });
+        btn_mark.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                LatLng mylocation = new LatLng(myLatitude,myLongitude);
+                mMap.addMarker(new MarkerOptions().position(mylocation).title("My Location"));
+            }
+        });
 
         btn_search.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+//
+//                List<Address> addressList = null;
+//
+//                String location = et_location.getText().toString().trim();
+//                if(location != null || !location.equals("")){
+//
+//                    Geocoder geocoder = new Geocoder(getApplicationContext());
+//                    try {
+//                        addressList = geocoder.getFromLocationName(location,1);
+//                    } catch (IOException e) {
+//                        e.printStackTrace();
+//                    }
+//
+//                    Address address = addressList.get(0);
+//
+//                    LatLng latLng = new LatLng(address.getLatitude(),address.getLongitude());
+//
+//                    mMap.addMarker(new MarkerOptions().position(latLng).title("Marker On Your Location"));
+//                    mMap.animateCamera(CameraUpdateFactory.newLatLng(latLng));
+//                }
 
-                List<Address> addressList = null;
-
-                String location = et_location.getText().toString().trim();
-                if(location != null || !location.equals("")){
-
-                    Geocoder geocoder = new Geocoder(getApplicationContext());
-                    try {
-                        addressList = geocoder.getFromLocationName(location,1);
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-
-                    Address address = addressList.get(0);
-
-                    LatLng latLng = new LatLng(address.getLatitude(),address.getLongitude());
-
-                    mMap.addMarker(new MarkerOptions().position(latLng).title("Marker On Your Location"));
-                    mMap.animateCamera(CameraUpdateFactory.newLatLng(latLng));
-                }
             }
         });
 
 
     }
+
 
     @Override
     protected void onResume() {
@@ -89,7 +148,12 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         // Add a marker in Sydney and move the camera
         LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        //.icon and further is for adding our own Marker At perticular Location.
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney").icon(BitmapDescriptorFactory.fromResource(R.mipmap.ic_launcher)));
+        // to Draw Polylines to map through given Coordinates.,use......
+        mMap.addPolyline(new PolylineOptions().add()
+
+        );
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             // TODO: Consider calling
@@ -99,8 +163,52 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             //                                          int[] grantResults)
             // to handle the case where the user grants the permission. See the documentation
             // for ActivityCompat#requestPermissions for more details.
-            return;
+            mMap.setMyLocationEnabled(true);
+        } else {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, MY_PERMISSION_FINE_LOCATION);
+            }
         }
-        mMap.setMyLocationEnabled(true);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        switch (requestCode) {
+            case MY_PERMISSION_FINE_LOCATION:
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                        // TODO: Consider calling
+                        //    ActivityCompat#requestPermissions
+                        // here to request the missing permissions, and then overriding
+                        //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                        //                                          int[] grantResults)
+                        // to handle the case where the user grants the permission. See the documentation
+                        // for ActivityCompat#requestPermissions for more details.
+                        mMap.setMyLocationEnabled(true);
+                    }
+
+                }else {
+                    Toast.makeText(getApplicationContext(),"This App Require Location Permission",Toast.LENGTH_LONG).show();
+                    finish();
+                }
+                break;
+        }
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
     }
 }
