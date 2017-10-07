0a23796dd039b81e256897a5179425c70d1bfc38
Ravneet Punia <ravneet.dtu@Gmail.com>
2017-10-07 19:47:27 +0530

    Image Compressor Added and Working Fine

--- Bitmap/app/src/main/java/in/ravneet/bitmap/MainActivity.java
+++ Bitmap/app/src/main/java/in/ravneet/bitmap/MainActivity.java
@@ -1,33 +1,37 @@
 package in.ravneet.bitmap;
 
+import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.media.Image;
 import android.net.Uri;
 import android.os.Bundle;
-import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.ImageView;
 import android.widget.Toast;
+import com.squareup.picasso.Picasso;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
+
+import id.zelory.compressor.Compressor;
 
 public class MainActivity extends AppCompatActivity {
     public static final int RESULT_LOAD_IMG = 101;
-    Uri imageUri;
+    public static final String TAG = "false";
     ImageView iv_original;
     ImageView iv_modified;
+    File actualImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,73 +56,19 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if (resultCode == RESULT_OK && requestCode == RESULT_LOAD_IMG) {
+        if (resultCode == RESULT_OK && requestCode == RESULT_LOAD_IMG && data != null) {
+            ImageView original = (ImageView) findViewById(R.id.iv_originalImage);
+            ImageView compressedImage = (ImageView) findViewById(R.id.iv_Lowsizeimage);
             try {
-                final Uri imageUri = data.getData();
-                final InputStream imageStream = getContentResolver().openInputStream(imageUri);
-                final Bitmap selectedImage = BitmapFactory.decodeStream(imageStream);
-                ((ImageView) findViewById(R.id.iv_originalImage)).setImageBitmap(selectedImage);
-            } catch (FileNotFoundException e) {
-                e.printStackTrace();
-                Toast.makeText(MainActivity.this, "Something went wrong", Toast.LENGTH_LONG).show();
-            }
-
-        }else {
-            Toast.makeText(MainActivity.this, "You haven't picked Image",Toast.LENGTH_LONG).show();
-        }
-    }
-
-    private String decodeFile(String path,int DESIREDWIDTH, int DESIREDHEIGHT) {
-        String strMyImagePath = null;
-        Bitmap scaledBitmap = null;
-
-        try {
-            // Part 1: Decode image
-            Bitmap unscaledBitmap = ScalingUtilities.decodeFile(path, DESIREDWIDTH, DESIREDHEIGHT, ScalingUtilities.ScalingLogic.FIT);
-
-            if (!(unscaledBitmap.getWidth() <= DESIREDWIDTH && unscaledBitmap.getHeight() <= DESIREDHEIGHT)) {
-                // Part 2: Scale image
-                scaledBitmap = ScalingUtilities.createScaledBitmap(unscaledBitmap, DESIREDWIDTH, DESIREDHEIGHT, ScalingUtilities.ScalingLogic.FIT);
-            } else {
-                unscaledBitmap.recycle();
-                return path;
-            }
-
-            // Store to tmp file
-
-            String extr = Environment.getExternalStorageDirectory().toString();
-            File mFolder = new File(extr + "/TMMFOLDER");
-            if (!mFolder.exists()) {
-                mFolder.mkdir();
-            }
-
-            String s = "tmp.png";
-
-            File f = new File(mFolder.getAbsolutePath(), s);
-
-            strMyImagePath = f.getAbsolutePath();
-            FileOutputStream fos = null;
-            try {
-                fos = new FileOutputStream(f);
-                scaledBitmap.compress(Bitmap.CompressFormat.JPEG, 75, fos);
-                fos.flush();
-                fos.close();
-            } catch (FileNotFoundException e) {
-
-                e.printStackTrace();
-            } catch (Exception e) {
-
+                actualImage = FileUtil.from(this, data.getData());
+                original.setImageBitmap(BitmapFactory.decodeFile(actualImage.getAbsolutePath()));
+//                actualSizeTextView.setText(String.format("Size : %s", getReadableFileSize(actualImage.length())));
+                File compressedImageFile = new Compressor(this).compressToFile(actualImage);
+                compressedImage.setImageBitmap(BitmapFactory.decodeFile(compressedImageFile.getAbsolutePath()));
+            } catch (IOException e) {
+                Toast.makeText(this, "Problem Occured", Toast.LENGTH_SHORT).show();
                 e.printStackTrace();
             }
-
-            scaledBitmap.recycle();
-        } catch (Throwable e) {
         }
-
-        if (strMyImagePath == null) {
-            return path;
-        }
-        return strMyImagePath;
-
     }
 }
